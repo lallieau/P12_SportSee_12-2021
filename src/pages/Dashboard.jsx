c9c14e7b9c4684add982e660f2aae75779a8b9dc
Lallie <lallie.audry@gmail.com>
2021-11-30 16:44:51 +0100

    create pages and routes, start create components

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import {DashboardHeader} from '../components/Dashboard/DashboardHeader'
+
+export const Dashboard = () => {
+    return (
+        <>
+            <h1>Dashboard</h1>
+            <DashboardHeader />
+            
+        </>
+        
+    )
+}
